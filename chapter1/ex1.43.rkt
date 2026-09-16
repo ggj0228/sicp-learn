4343e96bbb21907be380de4e3b132a0a2ea6ae76
wooyoungking <ggj0228@naver.com>
2026-09-17 00:19:34 +0900

    Solve SICP 1.43

---
+++ chapter1/ex1.43.rkt
@@ -0,0 +1,56 @@
+#lang sicp
+
+(define (average a b)
+  (/ (+ a b ) 2))
+
+(define tolerance 0.00001)
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (average-damp f x)
+  (lambda (x)
+    (average x (f x))))
+
+(define (deriv g)
+  (lambda (x)
+    (/ (- (g (+ x dx)) (g x))
+       dx)))
+(define dx 0.00001)
+
+
+(define (newton-transform g)
+  (lambda (x)
+    (- x (/ (g x) 
+            ((deriv g) x)))))
+
+(define (newtons-method g guess)
+  (fixed-point (newton-transform g) 
+               guess))
+
+
+(define (fixed-point-of-transform 
+         g transform guess)
+  (fixed-point (transform g) guess))
+
+
+(define (square x) (* x x))
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+(define (repeated f n)
+  (if (= n 1)
+      f
+      (compose f (repeated f (- n 1)))))
+
+((repeated square 2) 5)
+
+        
