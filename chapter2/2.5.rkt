d9ceeee774acb23529bedbbe3e6e1250b3a4b25c
wooyoungking <ggj0228@naver.com>
2026-09-22 15:09:23 +0900

    Solve Sicp 2.5

---
+++ chapter2/2.5.rkt
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (cons a b)
+  (* (expt 2 a)
+     (expt 3 b)))
+
+(define  (car n)
+  (if (= (remainder n 2) 0)
+      (+ 1 (car (/ n 2)))
+      0))
+
+(define (cdr n)
+  (if (= (remainder n 3) 0)
+      (+ 1 (cdr (/ n 3)))
+      0))
+
+(define x (cons 4 5))
+
+x
